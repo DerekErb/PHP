76dd15821b458a3f6186f3993472e4dc154de654
Derek Erb <derek@derekerb.solutions>
2026-04-14 17:58:40 +0200

    Change speed unit abbreviations in SpeedConversion
    Removed slashes from conversation strings
    They are unnecessary and were inconsistent (mph but km/h for example)

--- Conversions/SpeedConversion.php
+++ Conversions/SpeedConversion.php
@@ -8,9 +8,9 @@
  * https://www.asknumbers.com/SpeedConversion.aspx
  * Available units for conversion:
  * mph -> miles per hour
- * km/h -> kilometers per hour
- * m/s -> meters per second
- * ft/s -> feet per second
+ * kmh -> kilometers per hour
+ * ms -> meters per second
+ * fts -> feet per second
  * kn -> 1 knot which is equal to 1 nautical mile (1852 km/h)
  * The conversion is made using kilometers as base
  *
@@ -24,16 +24,16 @@ function convertSpeed(float $speed, string $unitFrom, string $unitTo)
 {
     $speedUnitsFrom = [
         'mph' => 1.609344,
-        'km/h' => 1,
-        'm/s' => 3.6,
-        'ft/s' => 1.097,
+        'kmh' => 1,
+        'ms' => 3.6,
+        'fts' => 1.097,
         'kn' => 1.852,
     ];
     $speedUnitsTo = [
         'mph' => 0.6213712,
-        'km/h' => 1,
-        'm/s' => 0.277778,
-        'ft/s' => 0.911344,
+        'kmh' => 1,
+        'ms' => 0.277778,
+        'fts' => 0.911344,
         'kn' => 0.539957,
     ];
     $availableUnits = array_keys($speedUnitsFrom);
